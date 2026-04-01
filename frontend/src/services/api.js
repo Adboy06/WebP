74d412f7b81972920bc034318451d890a6f29770
Aditya <adiboy6506@gmail.com>
2026-04-01 13:09:52 +0530

    groqified

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5001/api"
+  baseURL: "https://hearty-warmth-production-e0d9.up.railway.app/api"
 });
 
 // attach JWT token automatically
